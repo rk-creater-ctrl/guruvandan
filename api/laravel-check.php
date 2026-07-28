7aaacf020b2adcf66d91a7be9b82ee4cb786aa72
rk-creater-ctrl <ritikkushwaha3893@gmail.com>
2026-07-28 21:04:53 +0530

    Use Vercel tmp path for Laravel caches

--- api/laravel-check.php
+++ api/laravel-check.php
@@ -18,6 +18,18 @@ putenv('VIEW_COMPILED_PATH=/tmp/guruvandan/views');
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/guruvandan/views';
 $_SERVER['VIEW_COMPILED_PATH'] = '/tmp/guruvandan/views';
 
+foreach ([
+    'APP_PACKAGES_CACHE' => '/tmp/guruvandan/cache/packages.php',
+    'APP_SERVICES_CACHE' => '/tmp/guruvandan/cache/services.php',
+    'APP_CONFIG_CACHE' => '/tmp/guruvandan/cache/config.php',
+    'APP_ROUTES_CACHE' => '/tmp/guruvandan/cache/routes.php',
+    'APP_EVENTS_CACHE' => '/tmp/guruvandan/cache/events.php',
+] as $key => $value) {
+    putenv($key.'='.$value);
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
+
 $result = [
     'ok' => false,
     'step' => 'start',
